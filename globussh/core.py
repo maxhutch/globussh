ea8ab6e7ce26ced4d6aef793ca53482adaa1dbe2
Max Hutchinson <maxhutch@gmail.com>
2015-03-25 11:31:54 -0500

    Using `Popen.communicate` and `PIPE` to pass in transfer.
    This removes the `tmp.transfer` file, which wasn't multiprocessing
    safe.

--- globussh/core.py
+++ globussh/core.py
@@ -1,5 +1,4 @@
-from subprocess import call, check_output
-
+from subprocess import call, check_output, Popen, PIPE
 
 def globus_exists(path):
     foo = check_output(args=["/usr/bin/ssh", "globus", "ls {:s}".format(path)])
@@ -7,30 +6,31 @@ def globus_exists(path):
 
 
 def transfer_sync(transfer, label="globussh_sync"):
-    with open("tmp.transfer", "w") as f:
-        f.write(transfer)
     taskid = check_output(args=["/usr/bin/ssh",
                                 "globus",
                                 "transfer --generate-id"]
                           ).decode("utf-8")
-    with open("tmp.transfer", "r") as f:
-        call(args=["/usr/bin/ssh",
-                   "globus",
-                   "transfer",
-                   "-s 3 ",
-                   "--label={:s}".format(label),
-                   "--taskid={:s}".format(taskid)],
-             stdin=f)
+    p = Popen(args=["/usr/bin/ssh",
+                    "globus",
+                    "transfer",
+                    "-s 3 ",
+                    #"-g",
+                    "--label={:s}".format(label),
+                    "--taskid={:s}".format(taskid)],
+                    universal_newlines=True,
+                    stdin=PIPE)
+    p.communicate(input=transfer)
+    p.wait()
     call(args=["/usr/bin/ssh", "globus", "wait -q {:s}".format(taskid)])
 
 
 def transfer_async(transfer, label="globussh_async"):
-    with open("tmp.transfer", "w") as f:
-        f.write(transfer)
-    with open("tmp.transfer", "r") as f:
-        call(args=["/usr/bin/ssh",
-                   "globus",
-                   "transfer",
-                   "-s 3",
-                   "--label={:s}".format(label)],
-             stdin=f)
+    p = Popen(args=["/usr/bin/ssh",
+                    "globus",
+                    "transfer",
+                    "-s 3 ",
+                    "--label={:s}".format(label)],
+                    universal_newlines=True,
+                    stdin=PIPE)
+    p.communicate(input=transfer)
+    p.wait()
